99ddee02b52b9804078038768d7c1e02ad99c552
熙然 <970852638@qq.com>
2024-05-01 00:26:49 +0800

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -53,54 +53,21 @@ info = {
     }
 }
 
-def decode(text):
-    try:
-        # 尝试解析XML数据
-        root = tree.fromstring(text)
-    except tree.ParseError as e:
-        # 如果解析失败，打印错误信息
-        print("An error occurred while parsing XML:", e)
-        # 这里可以添加更多的错误处理逻辑，比如尝试修复数据或者退出函数
-        # return None 或者 re-raise 异常等
-        return None
-    manifest_node = root.find('.//manifest')
-    if manifest_node is None:
-        print("Error: manifest_node is None")
-        return
-    #print("text:",text)
-    #print("root:",root)
-    #print("manifest_node:",manifest_node)
-    manifest_version = manifest_node.get('version')
-    
-    package_node = root.find('.//package')
-    package_name = package_node.get('name')
-    package_size = int(package_node.get('size'))
-    package_sha1 = package_node.get('hash')
-    package_sha1 = base64.b64decode(package_sha1)
-    package_sha1 = package_sha1.hex()
-    package_sha256 = package_node.get('hash_sha256')
-
-    url_nodes = root.findall('.//url')
-    url = url_nodes[0].get('codebase') + package_name
-    print("package_name:",package_name)
-    print("package_size:",package_size)
-    print("package_sha1:",package_sha1)
-    print("package_sha256:",package_sha256)
-    print("url:",url)
-
-    
-    # url_prefixes = []
-    # for node in url_nodes:
-    #     url_prefixes.append(node.get('codebase') + package_name)
-
-    return {"version":manifest_version, "size":package_size, "sha1":package_sha1, "sha256":package_sha256, "url":url}
-    
 results = {}
 
-def version_tuple(v):
-    return tuple(map(int, (v.split("."))))
+# 解析响应头中的哈希值
+def get_hashes(headers):
+    hashes = {}
+    # 按逗号分割不同的哈希值
+    hash_parts = headers['X-Goog-Hash'].split(',')
+    for part in hash_parts:
+        # 分离哈希类型和哈希值
+        hash_type, hash_value = part.split('=')
+        # 由于哈希值是base64编码的，我们需要对其进行解码
+        decoded_value = base64.b64decode(hash_value).decode('utf-8')
+        hashes[hash_type] = decoded_value
+    return hashes
 
-#暂时没有想到办法较少post请求次数。
 def fetch():
     global results
     while True:
@@ -134,7 +101,8 @@ def fetch():
                     print(f"请求失败，状态码：{response.status_code}")
                     continue
             #获取下载链接
-            print("res.download_url:",res.url)
+            # print("res.download_url:",res.url)
+            results['arch']['k']['url'] = res.url
             
             # 从响应头中获取文件大小（字节）
             file_size_bytes_str = res.headers.get('X-Goog-Stored-Content-Length', '0')
@@ -142,43 +110,26 @@ def fetch():
             
             # 使用humansize函数获取用户友好的文件大小
             file_size_human = humansize(file_size_bytes)
-            
-            print(f"文件大小: {file_size_human}")
-                    
-            # if "release" in k:
-            #     data['label'] = "Release稳定版"
-            # if "esr" in k:
-            #     data['label'] = "Esr稳定版"
-            # elif "beta" in k:
-            #     data['label'] = "Beta测试版"
-            # elif "devedition" in k:
-            #     data['label'] = "Dev开发版"
-            # elif "nightly" in k:
-            #     data['label'] = "Nightly每夜版"
-            # print("res:",res)
-            #print("data2:",data)
-            #print("results:",results)
-            #下面的代码因为我把data.json的格式改了，所以执行有问题，且我想到一个问题，就是他这个判断是判断的stable版本有无更新，有更新才执行后面的代码，但是有可能stable没更新，其他版本有更新，这样就会导致更新不及时，所以索性注释掉这个代码，让他按github action设置的时间频率，每次执行的时候都重新生成data.json，而且注释这段代码github action执行后自己也会判断生成的文件有无变化，如果没变化则不自动更新生成
-            #if version_tuple(data['version']) < version_tuple():
-            #    print("ignore", k, data['version'])
-            #    continue
-            #print("data['time']:",data['time'])
-            #print("results['data']['win_x86']1:",results['data']['win_x86'])
-            # if version_tuple(results['data'][arch][k]['version']) < version_tuple(data['version']):
-            #     print("results['data'][arch][k]['updatetime']:",arch,k,results['data'][arch][k]['updatetime'])
-            #     data['updatetime'] = int(datetime.now().timestamp() * 1000)
-            #     print("results['data'][arch][k]['version']:",arch,k,results['data'][arch][k]['version'])
-            #     print("data['version']:",data['version'])
-            #     print("data['updatetime']:",data['updatetime'])
-                #如果版本有更新则输出更新的data的所有内容，如果版本号没更新就跳出本次循环，这样写是因为版本号请求有时候会变，一会变成新版本，一会又调到老版本了。这样写能保证data.json文件中的版本号是最新的。
-                # results['data'][arch].update({k: data})
-                # print("updated results['data'][arch][k]['version']:",k,results['data'][arch][k]['version'])
-            # else:
-            #     continue
-                # data['updatetime'] = results['data']['win_x86'][k]['updatetime']
+            results['arch']['k']['size'] = file_size_human
+            # print(f"文件大小: {file_size_human}")
 
-    #data.json里面win7的数据是固定不变的
-    
+            # 获取哈希值
+            hashes = get_hashes(res.headers)
+            
+            # 打印结果
+            print(f"CRC32C Hash: {hashes.get('crc32c')}")
+            print(f"MD5 Hash: {hashes.get('md5')}")
+            
+            if "release" in k:
+                results['arch']['k']['label'] = "Release稳定版"
+            if "esr" in k:
+                results['arch']['k']['label'] = "Esr稳定版"
+            elif "beta" in k:
+                results['arch']['k']['label'] = "Beta测试版"
+            elif "devedition" in k:
+                results['arch']['k']['label'] = "Dev开发版"
+            elif "nightly" in k:
+                results['arch']['k']['label'] = "Nightly每夜版"
     #获取最新时间并更新到data.json的time字典中
     results.update({'time': int(datetime.now().timestamp() * 1000)})
     with open('data.json', 'w') as f:
@@ -194,4 +145,3 @@ def humansize(nbytes):
     return '%s %s' % (f, suffixes[i])
 
 fetch()
-
