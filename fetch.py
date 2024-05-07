675fc665e15269e08fa59cdffbe993ce4fc4211e
熙然 <970852638@qq.com>
2024-05-08 00:52:32 +0800

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -13,8 +13,8 @@ headers = {
 while True:
     # 发送HTTP GET请求
     response = requests.get(url, headers=headers)
-    # print("response.text:",response.text)
-    # print("response.status_code:",response.status_code)
+    print("response.text:",response.text)
+    print("response.status_code:",response.status_code)
     # 确保请求成功
     if response.status_code == 200:
         # 尝试将响应内容解码为utf-8，如果失败则使用默认编码
